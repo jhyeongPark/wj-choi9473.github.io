500824296909094af82f088c51e6bd7f93ba6eff
wj-choi9473 <52997573+wj-choi9473@users.noreply.github.com>
2020-08-26 23:14:52 +0900

    update

--- _posts/2020-08-26-postexample.md
+++ _posts/2020-08-26-postexample.md
@@ -10,7 +10,7 @@ tags:
 
 ---
 
-```This formula $f(x) = x^2$ is an example.````
+```This formula $f(x) = x^2$ is an example.```  
 This formula $f(x) = x^2$ is an example.  
 
 ```$$
@@ -19,8 +19,4 @@ This formula $f(x) = x^2$ is an example.
 \lim_{x\to 0}{\frac{e^x}{2}}={\frac{1}{2}}
 $$```  
 
-$$
-\lim_{x\to 0}{\frac{e^x-1}{2x}}
-\overset{\left[\frac{0}{0}\right]}{\underset{\mathrm{H}}{=}}
-\lim_{x\to 0}{\frac{e^x}{2}}={\frac{1}{2}}
-$$
+$$\lim_{x\to 0}{\frac{e^x-1}{2x}}\overset{\left[\frac{0}{0}\right]}{\underset{\mathrm{H}}{=}}\lim_{x\to 0}{\frac{e^x}{2}}={\frac{1}{2}}$$
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Example of Post",
-        "excerpt":"This formula $f(x) = x^2$ is an example.` This formula $f(x) = x^2$ is an example.   $$ \\lim_{x\\to 0}{\\frac{e^x-1}{2x}} \\overset{\\left[\\frac{0}{0}\\right]}{\\underset{\\mathrm{H}}{=}} \\lim_{x\\to 0}{\\frac{e^x}{2}}={\\frac{1}{2}} $$   \\[\\lim_{x\\to 0}{\\frac{e^x-1}{2x}} \\overset{\\left[\\frac{0}{0}\\right]}{\\underset{\\mathrm{H}}{=}} \\lim_{x\\to 0}{\\frac{e^x}{2}}={\\frac{1}{2}}\\] ","categories": ["Example"],
+        "excerpt":"This formula $f(x) = x^2$ is an example.  This formula $f(x) = x^2$ is an example.   $$ \\lim_{x\\to 0}{\\frac{e^x-1}{2x}} \\overset{\\left[\\frac{0}{0}\\right]}{\\underset{\\mathrm{H}}{=}} \\lim_{x\\to 0}{\\frac{e^x}{2}}={\\frac{1}{2}} $$   \\[\\lim_{x\\to 0}{\\frac{e^x-1}{2x}}\\overset{\\left[\\frac{0}{0}\\right]}{\\underset{\\mathrm{H}}{=}}\\lim_{x\\to 0}{\\frac{e^x}{2}}={\\frac{1}{2}}\\] ","categories": ["Example"],
         "tags": ["Example","MathJax","Latex"],
         "url": "http://localhost:4000/example/postexample/",
         "teaser": null
