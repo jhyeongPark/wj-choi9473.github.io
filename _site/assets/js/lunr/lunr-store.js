256b658822e8226191ac07d0160badd3de81864e
wj-choi9473 <52997573+wj-choi9473@users.noreply.github.com>
2020-08-26 23:28:55 +0900

    update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
         "title": "Example of Post",
-        "excerpt":"This formula $f(x) = x^2$ is an example.  This formula $f(x) = x^2$ is an example.   $$ \\lim_{x\\to 0}{\\frac{e^x-1}{2x}} \\overset{\\left[\\frac{0}{0}\\right]}{\\underset{\\mathrm{H}}{=}} \\lim_{x\\to 0}{\\frac{e^x}{2}}={\\frac{1}{2}} $$   \\[\\lim_{x\\to 0}{\\frac{e^x-1}{2x}}\\overset{\\left[\\frac{0}{0}\\right]}{\\underset{\\mathrm{H}}{=}}\\lim_{x\\to 0}{\\frac{e^x}{2}}={\\frac{1}{2}}\\] ","categories": ["Example"],
-        "tags": ["Example","MathJax","Latex"],
+        "excerpt":"This formula $f(x) = x^2$ is an example.  This formula $f(x) = x^2$ is an example.   \\[1+1\\] ","categories": ["Example"],
+        "tags": [],
         "url": "http://localhost:4000/example/postexample/",
         "teaser": null
       }]
